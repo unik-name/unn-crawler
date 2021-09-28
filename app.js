aa7fe67a98236102191c9462458f3d8c3884c7eb
Damien Lecan <damien.lecan@spacelephant.org>
2021-09-28 11:23:11 +0200

    style: formatting

--- app.js
+++ app.js
@@ -1,99 +1,120 @@
-const Crawler = require('./src/crawler')
-const { URL } = require('url')
-const { orderBy } = require('lodash/collection')
+const Crawler = require("./src/crawler");
+const { URL } = require("url");
+const { orderBy } = require("lodash/collection");
 
-const crawler = new Crawler()
-const args = process.argv.slice(2)
+const crawler = new Crawler();
+const args = process.argv.slice(2);
 
 const report = (crawler) => {
-  const blockStats = {}
-  const versionStats = {}
+  const blockStats = {};
+  const versionStats = {};
 
-  const ipNodes = orderBy(Object.keys(crawler.nodes))
+  const ipNodes = orderBy(Object.keys(crawler.nodes));
 
   for (const ipNode of ipNodes) {
     const item = crawler.nodes[ipNode];
     if (item.height === undefined || item.id === undefined) {
-      continue
+      continue;
     }
 
     if (blockStats[item.height]) {
-      blockStats[item.height].count += 1
-      blockStats[item.height].ids[item.id].hashCount += 1
-      blockStats[item.height].ids[item.id].ips.push(ipNode)
+      blockStats[item.height].count += 1;
+      blockStats[item.height].ids[item.id].hashCount += 1;
+      blockStats[item.height].ids[item.id].ips.push(ipNode);
     } else {
-      blockStats[item.height] = {}
-      blockStats[item.height].count = 1
-      blockStats[item.height].height = item.height
+      blockStats[item.height] = {};
+      blockStats[item.height].count = 1;
+      blockStats[item.height].height = item.height;
       // todo block ids
-      blockStats[item.height].ids = {}
-      blockStats[item.height].ids[item.id] = {}
-      blockStats[item.height].ids[item.id].hashCount = 1
-      blockStats[item.height].ids[item.id].ips = [ipNode]
+      blockStats[item.height].ids = {};
+      blockStats[item.height].ids[item.id] = {};
+      blockStats[item.height].ids[item.id].hashCount = 1;
+      blockStats[item.height].ids[item.id].ips = [ipNode];
     }
 
     if (versionStats[item.version]) {
-      versionStats[item.version].count += 1
-      versionStats[item.version].ips.push(ipNode)
+      versionStats[item.version].count += 1;
+      versionStats[item.version].ips.push(ipNode);
     } else {
       versionStats[item.version] = {
         count: 1,
         version: item.version,
-        ips: [ipNode]
-      }
+        ips: [ipNode],
+      };
     }
   }
 
-  const nodes = Object.values(crawler.nodes)
+  const nodes = Object.values(crawler.nodes);
 
-  const allDelays = nodes.filter(item => item.latency).map(item => item.latency)
-  const averageDelay = (allDelays.reduce((a, b) => a + b, 0) / allDelays.length).toFixed(2)
-  const maxDelay = Math.max(...allDelays)
-  const minDelay = Math.min(...allDelays)
+  const allDelays = nodes
+    .filter((item) => item.latency)
+    .map((item) => item.latency);
+  const averageDelay = (
+    allDelays.reduce((a, b) => a + b, 0) / allDelays.length
+  ).toFixed(2);
+  const maxDelay = Math.max(...allDelays);
+  const minDelay = Math.min(...allDelays);
 
-  console.log('===========================================')
-  console.log(`All nodes: ${Object.keys(crawler.nodes).length}`)
-  console.log(`Nodes online: ${crawler.heights.length}`)
-  console.log(`Nodes offline: ${Object.keys(crawler.nodes).length - crawler.heights.length}`)
+  console.log("===========================================");
+  console.log(`All nodes: ${Object.keys(crawler.nodes).length}`);
+  console.log(`Nodes online: ${crawler.heights.length}`);
+  console.log(
+    `Nodes offline: ${
+      Object.keys(crawler.nodes).length - crawler.heights.length
+    }`
+  );
 
   // height/block stats
-  console.log('')
-  console.log('Height and block stats:')
-  for (const stat of orderBy(Object.values(blockStats), ['height'], ['desc'])) {
-    console.log(`  ${stat.height} with ${stat.count} nodes. Block hashes:`)
+  console.log("");
+  console.log("Height and block stats:");
+  for (const stat of orderBy(Object.values(blockStats), ["height"], ["desc"])) {
+    console.log(`  ${stat.height} with ${stat.count} nodes. Block hashes:`);
     for (const hash in stat.ids) {
-      console.log(`    - ${hash} (${stat.ids[hash].hashCount} nodes - ${JSON.stringify(stat.ids[hash].ips)})`)
+      console.log(
+        `    - ${hash} (${stat.ids[hash].hashCount} nodes - ${JSON.stringify(
+          stat.ids[hash].ips
+        )})`
+      );
     }
   }
 
   // version stats
-  console.log('')
-  console.log('Version stats:')
-  for (const stat of orderBy(Object.values(versionStats), ['version'], ['desc'])) {
-    console.log(`  - ${stat.version} on ${stat.count} nodes - ${JSON.stringify(stat.ips)}`)
+  console.log("");
+  console.log("Version stats:");
+  for (const stat of orderBy(
+    Object.values(versionStats),
+    ["version"],
+    ["desc"]
+  )) {
+    console.log(
+      `  - ${stat.version} on ${stat.count} nodes - ${JSON.stringify(stat.ips)}`
+    );
   }
 
   // delay stats
-  console.log('')
-  console.log('Delay')
-  console.log(`  Avg: ${averageDelay}ms`)
-  console.log(`  Min: ${minDelay}ms`)
-  console.log(`  Max: ${maxDelay}ms`)
+  console.log("");
+  console.log("Delay");
+  console.log(`  Avg: ${averageDelay}ms`);
+  console.log(`  Min: ${minDelay}ms`);
+  console.log(`  Max: ${maxDelay}ms`);
 
   console.log(`${ipNodes.length} IPs:`);
   console.log("  " + JSON.stringify(ipNodes));
 
-  console.log('------------------------------------------')
-  console.log(`Finished scanning in ${new Date() - crawler.startTime}ms`)
+  console.log("------------------------------------------");
+  console.log(`Finished scanning in ${new Date() - crawler.startTime}ms`);
 
-  process.exit(0)
-}
+  process.exit(0);
+};
 
-const node = { ip: undefined, port: undefined }
+const node = { ip: undefined, port: undefined };
 if (args.length === 1) {
-  const url = new URL(args[0])
-  node.ip = url.hostname
-  node.port = url.port
+  const url = new URL(args[0]);
+  node.ip = url.hostname;
+  node.port = url.port;
 }
 
-crawler.run(node).then(report).catch(err => console.error(err))
+crawler
+  .run(node)
+  .then(report)
+  .catch((err) => console.error(err));
